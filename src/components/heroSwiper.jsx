905b7daa8db6c39fdea978fbb5cf99acff67f6d2
Emmanuel Balogun <emmanx25@gmail.com>
2024-06-14 07:59:53 +0100

    Update hero component layout and styles

--- src/components/heroSwiper.jsx
+++ src/components/heroSwiper.jsx
@@ -14,56 +14,58 @@ import image5 from "../assets/swip5.png";
 
 const HeroSwiper = () => {
   return (
-    <div className="w-[975px] pt-[27px] pr-[9rem] relative">
-      <Swiper
-        modules={[EffectFade, Pagination, Autoplay]}
-        spaceBetween={30}
-        slidesPerView={1}
-        pagination={{
-          clickable: true,
-          el: ".custom-pagination",
-          renderBullet: (index, className) => {
-            return `<span class="${className} custom-bullet"></span>`;
-          },
-        }}
-        effect="fade"
-        fadeEffect={{ crossFade: true }}
-        autoplay={{
-          delay: 400, // Adjust delay as needed
-          disableOnInteraction: false,
-        }}
-        loop={true}
-        className="mySwiper"
-      >
-        <SwiperSlide>
-          <div className="swiper-slide-content">
-            <img src={image1} alt="Slide 1" className="image-cover" />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="swiper-slide-content">
-            <img src={image2} alt="Slide 2" className="image-cover" />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="swiper-slide-content">
-            <img src={image3} alt="Slide 3" className="image-cover" />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="swiper-slide-content">
-            <img src={image4} alt="Slide 4" className="image-cover" />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="swiper-slide-content">
-            <img src={image5} alt="Slide 5" className="image-cover" />
-          </div>
-        </SwiperSlide>
-      </Swiper>
-      {/* Custom Pagination */}
-      <div className="custom-pagination mt-4 flex justify-center"></div>
-    </div>
+    <>
+      <div className="w-[830px] flex flex-col items-start flex-shrink-0 pt-[27px]">
+        <Swiper
+          modules={[EffectFade, Pagination, Autoplay]}
+          spaceBetween={30}
+          slidesPerView={1}
+          pagination={{
+            clickable: true,
+            el: ".custom-pagination",
+            renderBullet: (index, className) => {
+              return `<span class="${className} custom-bullet"></span>`;
+            },
+          }}
+          effect="fade"
+          fadeEffect={{ crossFade: true }}
+          autoplay={{
+            delay: 300, // Adjust delay as needed
+            disableOnInteraction: false,
+          }}
+          loop={true}
+          className="mySwiper h-[425px] w-full"
+        >
+          <SwiperSlide>
+            <div className="swiper-slide-content">
+              <img src={image1} alt="Slide 1" className="h-[425px] w-[973px]" />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="swiper-slide-content">
+              <img src={image2} alt="Slide 2" className="h-[425px] w-[973px]" />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="swiper-slide-content">
+              <img src={image3} alt="Slide 3" className="h-[425px] w-[973px]" />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="swiper-slide-content">
+              <img src={image4} alt="Slide 4" className="h-[425px] w-[973px]" />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="swiper-slide-content">
+              <img src={image5} alt="Slide 5" className="h-[425px] w-[973px]" />
+            </div>
+          </SwiperSlide>
+          {/* Custom Pagination */}
+        </Swiper>
+        <div className="custom-pagination mt-4 flex justify-center"></div>
+      </div>
+    </>
   );
 };
 
