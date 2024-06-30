3dac71143bc5a733f6937e432637a14388251849
Emmanuel Balogun <emmanx25@gmail.com>
2024-06-30 09:50:47 +0100

    update pictures and layout for signup and verification page

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,21 +1,25 @@
 import React from "react";
-import sign from "../assets/sign.png";
+import sign from "../assets/signup.png";
+import logo from "../assets/eazzi_logo.svg";
 import mail from "../assets/mail.png";
 import SignupWithGoogleorLogin from "../components/SignupWithGoogleorLogin";
 
 const Signup = () => {
   return (
-    <div className="absolute z-50 bg-white w-full pb-36">
-      <div className="w-full flex flex-col md:flex-row items-center justify-between px-3 md:px-10 lg:px-28">
-        <div className="w-full h-full hidden md:flex">
-          <img src={sign} className="" alt="" />
+    <div className="absolute z-50 bg-white w-full pb-36 md:pb-0">
+      <div className="w-full flex flex-col md:flex-row items-center justify-between md:px-0">
+        <div className="hidden md:flex w-full h-[800px] flex-grow">
+          <img src={sign} className="w-full h-full object-cover" alt="Signup" />
         </div>
 
-        <div className="w-full mt-[65px] md:mt-[100px]">
-          <h2 className="text-[24px] font-tekInter text-[#4F4F4F] leading-[30px] font-[700]">
+        <div className="w-full mt-[65px] md:mt-[100px] md:px-10 lg:px-20">
+          <div className="w-full h-[100px] bg-[#1843E2] md:hidden mt-[-70px] px-3">
+            <img src={logo} className="pb-8 pt-[29px] h-[104px]" alt="" />
+          </div>
+          <h2 className="text-[24px] font-tekInter text-[#4F4F4F] leading-[30px] font-[700] px-3 mt-10">
             Sign up
           </h2>
-          <form action="" className="mt-[32px] flex flex-col gap-[24px]">
+          <form action="" className="mt-[32px] flex flex-col gap-[24px] px-3">
             <input
               type="text"
               className="text-[#828282] h-[53px] py-[26px] px-[16px] border-[1px] border-[#969696] outline-none w-full rounded-[8px]"
@@ -40,7 +44,11 @@ const Signup = () => {
                 placeholder="Email address"
                 required
               />
-              <img src={mail} className="absolute top-[13px] left-[10.3px]" alt="" />
+              <img
+                src={mail}
+                className="absolute top-[13px] left-[10.3px]"
+                alt="Mail icon"
+              />
             </div>
 
             <input
@@ -51,7 +59,12 @@ const Signup = () => {
               required
             />
 
-            <button type="submit" className="bg-[#1843E2] rounded-[8px] shadow-btn text-white text-center text-[16px] font-tekInter font-[600] leading-[24px] mt-[38px] py-[10px] px-[18px]">Continue</button>
+            <button
+              type="submit"
+              className="bg-[#1843E2] rounded-[8px] shadow-btn text-white text-center text-[16px] font-tekInter font-[600] leading-[24px] mt-[38px] py-[10px] px-[18px]"
+            >
+              Continue
+            </button>
           </form>
 
           <SignupWithGoogleorLogin />
